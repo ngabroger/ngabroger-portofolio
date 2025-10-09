19399a7b1e705c07f05f30ea1e587c61fdb9bd4f
ngabroger <arbination13@gmail.com>
2025-10-09 07:48:49 +0700

    Refactor code structure for improved readability and maintainability

--- src/app/components/HireMeSection.tsx
+++ src/app/components/HireMeSection.tsx
@@ -9,21 +9,21 @@ export default function HireMeSection() {
       <div className="flex gap-1 md:gap-5 items-center justify-center">
         <a
           href="mailto:dev.ngabroger@gmail.com"
-          className="bg-secondary text-sm text-white py-2 px-4 rounded-md"
+          className="bg-secondary text-sm text-white py-2 px-4 rounded-md hover:bg-neutral-950 transition-colors duration-300"
         >
           <FaMailBulk className="inline mr-2" />
           <span>Get in Touch</span>
         </a>
         <a
           href="https://www.linkedin.com/in/ngabroger/"
-          className="bg-secondary text-sm text-white py-2 px-4 rounded-md"
+          className="bg-secondary text-sm text-white py-2 px-4 rounded-md hover:bg-neutral-950 transition-colors duration-300"
         >
           <FaLinkedin className="inline mr-2" />
           <span>Linkedin</span>
         </a>
         <a
           href="https://drive.google.com/file/d/1cBzFLH42YglI8xqW8RSV1ZG7tpDFmBxN/view"
-          className="bg-secondary text-sm text-white py-2 px-4 rounded-md"
+          className="bg-secondary text-sm text-white py-2 px-4 rounded-md hover:bg-neutral-950 transition-colors duration-300"
         >
           <FaFile className="inline mr-2" />
           <span>Download CV</span>
