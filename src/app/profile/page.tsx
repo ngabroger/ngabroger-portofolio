9f2667bfed74d2f8a7c22d63cfc86eb1091a0edd
ngabroger <arbination13@gmail.com>
2025-12-22 14:01:49 +0700

    feat: remove unused sections and components from profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,4 @@
 'use client';
-import { useEffect } from 'react';
-import MainSection from './section/main';
-import SkillSection from './section/skill';
-import ProfileAnimationController from '@/components/animate/profile-animation-controller';
-import { useProfileNavigate } from '@/components/animate/animate-navigate-provider';
-import { div } from 'framer-motion/client';
 import { CardMain } from '@/components/ui/card-first-main';
 import { CardSecondaryMain } from '@/components/ui/card-secondary-main';
 
